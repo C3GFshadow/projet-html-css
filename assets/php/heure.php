2a3ecb78224810b873d0b57413967789418ea03c
C3GFshadow <dim.zarkyx@gmail.com>
2024-10-04 17:29:00 +0200

    first commit

---
+++ assets/php/heure.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set('Europe/Paris'); // Définit le fuseau horaire
+$heure = date('H:i:s'); // Récupère l'heure au format heures:minutes:secondes
+echo "L'heure actuelle est : " . $heure;
+?>
